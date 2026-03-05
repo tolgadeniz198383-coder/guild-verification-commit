82126c4d45fc0097defcaf0cad8fe31d18b86003
tolgadeniz198383-coder <tolgadeniz198383@gmail.com>
2026-03-05 18:41:42 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:40:31.048Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T15:41:41.716Z
+Commit: Fix CSS styling bug on mobile devices
